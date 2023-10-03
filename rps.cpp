4426718904a4fd430b5e51ccdb5e477b4a4fa9d8
TheReignEditz <132263693+TheReignEditz@users.noreply.github.com>
2023-10-03 21:56:54 +0530

    Update rps.cpp

--- rps.cpp
+++ rps.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <ctime>
 #include <cctype>
-using std::cout,std::endl,std::cin;
+using std::cout;
+using std::endl;
+using std::cin;
 
 char rps[6] = {'p','s','r','p','s','r'};
 char userChoice;
+
 char userDecision(){
     char choice;
-    cout << "Enter r for Rock, p for Paper and s for Scissors or anything else to exit." << endl;
+    cout << "Enter r for Rock, p for Paper and s for Scissors or anything else to exit:";
     cin >> choice;
     return tolower(choice);
 
@@ -17,10 +20,11 @@ int main(){
     int random,run = 1;
     while(run){
         srand(time(0));
+        random =  rand() % 6;
         cout << "WELCOME TO ROCK,PAPER AND SCISSORS" << endl;
         userChoice = userDecision();
-        random =  rand() % 7;
         char computerChoice = rps[random];
+
         if(computerChoice == userChoice){
             cout << "DRAW!!!!" << endl;
             cout << "You picked: " << userChoice << endl;
